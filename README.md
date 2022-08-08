4f9b1da56a6b04b2183ba6a64818626aa122d082
Davina Leong <davina.leong.sy@gmail.com>
2022-08-08 18:00:30 +0800

    Added blur effect

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# proj-blur-effect-on-image-load
- Blur loading effect by Traversy Media
+# Blur Effect on Image Load
+
+Following a tutorial by Traversy Media [here](https://www.youtube.com/watch?v=JkeyKeK3V24).
+
+Demo link coming soon!
--- scripts/main.js
+++ scripts/main.js
@@ -10,5 +10,12 @@ function blur() {
   if (progress >= 100) {
     clearInterval(interval);
   }
-  loadingText.innerHTML = `${progress}%`;
+  loadingText.innerText = `${progress}%`;
+  loadingText.style.opacity = scale(progress, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(progress, 0, 100, 30, 0)}px)`;
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale(num, in_min, in_max, out_min, out_max) {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 }
